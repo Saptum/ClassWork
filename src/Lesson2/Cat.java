e1b22b8b60b9dcefbef3f302a6d607e675fab7c0
Saptum <nikazavgorodniaya@gmail.com>
2020-02-22 13:51:13 +0200

    dragon

---
+++ src/Lesson2/Cat.java
@@ -0,0 +1,10 @@
+package Lesson2;
+
+public class Cat {
+    String name;
+    int age;
+
+    public void run() {
+        System.out.println("CAT RUN");
+    }
+}
---
+++ src/Lesson2/Main.java
@@ -0,0 +1,15 @@
+package Lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat =new Cat();
+        cat.age = 5;
+        cat.name ="soda";
+
+        System.out.println(cat);
+        System.out.println(cat.age);
+        System.out.println(cat.name);
+
+        cat.run();
+    }
+}
